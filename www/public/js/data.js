91cef030d1fb503cbe63b6762608d1b5a0136339
Jussi Kosunen <deathcarrot@gmail.com>
2011-02-19 00:29:03 +0200

    JSON fetch now works

--- www/public/js/data.js
+++ www/public/js/data.js
@@ -10,7 +10,7 @@ fetch:function(type,name,callback,depth,force) {
         if(json.error) {
             console.error("Error fetching JSON: "+json.error);
         } else {
-            callback(json);
+            callback(this._processJSON(json));
         }
     },this));
 },
@@ -19,5 +19,39 @@ get:function(type,name) {
 },
 remove:function(type,name) {
     // removes the unit and any associations it's linked to
+    console.error("DataRepository.remove not implemented");
+},
+_processJSON:function(json) {
+    var map = []; // maps json index to final object
+    for(var i=0;i<json.length;i++) {
+        var j = json[i];
+        var id = j[0]+"!"+j[1]; // Type!Name
+
+        if(!this[id]) { // prepare object if it doesn't exist
+            this[id] = {type:j[0],name:j[1],assoc:{},reverse:{}};
+        }
+        map.push(this[id]);
+    }
+    for(var i=0;i<json.length;i++) {
+        var j = json[i];
+        var id = j[0]+"!"+j[1];
+        var u = this[id];
+        
+        for(var a in j[2]) { // a = association name
+            var acc = j[2][a]; // acc = association target array
+            if(!u.assoc[a]) { // prep for data
+                u.assoc[a] = {};
+            }
+            for(var t=0;t<acc.length;t++) {
+                var at = map[acc[t]]; // association target
+                u.assoc[a][at.type+"!"+at.name] = at;
+                if(!at.reverse[a]) { // prep reverse
+                    at.reverse[a] = {};
+                }
+                at.reverse[a][u.type+"!"+u.name] = u;
+            }
+        }
+    }
+    return map[0];
 }
 });
