d0128ff47a34faa2b60eb0189a833f91e17d1613
Jussi Kosunen <deathcarrot@gmail.com>
2011-02-18 21:12:09 +0200

    JSONReply and more notes

---
+++ www/json.rb
@@ -0,0 +1,5 @@
+class JSONReply
+    def JSONReply.reply(context,query)
+        '{"foo":1,"bar":"two"}'
+    end
+end
--- www/unifide.rb
+++ www/unifide.rb
@@ -2,16 +2,17 @@ $LOAD_PATH << './model'
 require 'rubygems'
 require 'sinatra'
 require 'model'
+require 'json'
 
 class Unifide < Sinatra::Base
-    post '/json' do
-        response.headers['Content-type'] = 'application/json'
-        # might want to use the /json path for AJAX JSON requests
-        '{"a":1,"b":"two"}'
-    end
-
     get '/' do
         erb :index
+    end
+
+    get '/json/:context/:query' do
+        #response.headers['Content-type'] = 'application/json'
+        response.headers['Content-type'] = 'text/plain'
+        JSONReply.reply(:context,:query)
     end
 
     get '/units' do
