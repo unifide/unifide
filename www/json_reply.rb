3c57c92cc26bdb5f2f889ae2494538a1a807d0a4
Chris Franklin <mr.chris.franklin@gmail.com>
2011-03-02 22:31:31 +0000

    Factored out code for getting the different stages of the hierachy into several fantastical methods.

--- www/json_reply.rb
+++ www/json_reply.rb
@@ -48,63 +48,82 @@ class JSONReply
 	end
         hm.to_json
     end
-    
-    def self.get_unit(user, project_name, type_name, name, depth)
-        hm = {:success => false}
+
+    def self.process_project(user, project_name, hm)
         project = Project.find_by_short_name project_name
 	if !project.nil?
 	    #The project exists
             if user_can_see_project? user, project
 		#The user can access the project
-		unit_type = UnitType.find_by_name(type_name)
-		if !unit_type.nil?
-		    #The unit type exists
-		    unit = find_unit project, unit_type, name
-		    if !unit.nil?
-			#The unit exists
-			#Let's make a hash!
-			hm[:success] = true
-			hm[:depth] = depth
-			units = unit.get_neighbours depth
-			assocs = Association.get_relationships(units)
-			hm[:units] = {}
-			#units =>
-			Project.where(:id => units.collect{|u| u.project_id}).each do |p|
-			    hm[:units][p.short_name] = {}
-			    #<project> =>
-			    UnitType.where(:id => units.collect {|u| u.unit_type_id}).each do |ut|
-				hm[:units][p.short_name][ut.name] = []
-				#<unit type> => [list of units]
-				units.select {|u| u.project.short_name == p.short_name and u.unit_type_id == ut.id}.each do |u|
-				    as = {}
-				    #<association type> => [unit IDs]
-				    AssociationType.where(:id => assocs.select {|a| a.from_id == u.id }.collect {|a| a.association_type_id}).each do |at|
-					as[at.name] = u.from_associations.select {|a| a.association_type_id == at.id }.collect{|a| units.index(a.to)}
-				    end
-				    tus = {}
-				    #<text unit type> => [content]
-				    u.text_units.each do |tu|
-					tus[tu.text_unit_type.name] = tu.text
-				    end
-				    #[id,name,associations,content]
-				    hm[:units][p.short_name][ut.name] << [units.index(u), u.value, as, tus]
-				end
-			    end
-			end
-			#ping! hash is done
-		    else
-			hm[:errors] = [@@error_messages[:no_unit]]
-		    end
-                else
-		    hm[:errors] = [@@error_messages[:no_unit_type]]
-	        end
+		yield project
             else
 	        hm[:errors] = [@@error_messages[:permission]]
 	    end
 	else
 	    hm[:errors] = [@@error_messages[:no_project]]
 	end
-        hm.to_json
+    end
+
+    def self.process_unit_type(user, project_name, type_name, hm)
+	self.process_project user, project_name, hm do |project|
+	    unit_type = UnitType.find_by_name type_name
+	    if !unit_type.nil?
+		yield project, unit_type
+	    else
+		hm[:errors] = [@@error_messages[:no_unit_type]]
+	    end
+	end
+    end
+
+    def self.process_unit(user, project_name, type_name, name, hm)
+	self.process_unit_type user, project_name, type_name, hm do |project, unit_type|
+	    #The unit type exists
+	    unit = find_unit project, unit_type, name
+	    if !unit.nil?
+		yield project, unit_type, unit
+	    else
+		hm[:errors] = [@@error_messages[:no_unit]]
+	    end
+	end
+    end
+    
+    def self.get_unit(user, project_name, type_name, name, depth)
+        hm = {:success => false}
+	self.process_unit(user, project_name, type_name, name, hm) do |project, unit_type, unit|
+	    #Let's make a hash!
+	    hm[:success] = true
+	    hm[:depth] = depth
+	    units = unit.get_neighbours depth
+	    assocs = Association.get_relationships(units)
+	    hm[:units] = {}
+	    #units =>
+	    Project.where(:id => units.collect{|u| u.project_id}).each do |p|
+		hm[:units][p.short_name] = {}
+		#<project> =>
+		UnitType.where(:id => units.collect {|u| u.unit_type_id}).each do |ut|
+		    hm[:units][p.short_name][ut.name] = []
+		    #<unit type> => [list of units]
+		    units.select {|u| u.project.short_name == p.short_name and u.unit_type_id == ut.id}.each do |u|
+			as = {}
+			#<association type> => [unit IDs]
+			AssociationType.where(:id => assocs.select {|a| a.from_id == u.id }.collect {|a| a.association_type_id}).each do |at|
+			    as[at.name] = u.from_associations.select {|a| a.association_type_id == at.id }.collect{|a| units.index(a.to)}
+			end
+			tus = {}
+			#<text unit type> => [content]
+			u.text_units.each do |tu|
+			    tus[tu.text_unit_type.name] = tu.text
+			end
+			#[id,name,associations,content]
+			hm[:units][p.short_name][ut.name] << [units.index(u), u.value, as, tus]
+		    end
+		end
+	    end
+	end
+	hm.to_json
+    end
+
+    def self.create_unit(user, project, type, name)
     end
 
     private
